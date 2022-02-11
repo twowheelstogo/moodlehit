3dabb0aecdf0cf58de8119c5d5ba4b140632a876
Emanuelztrada <emanuelestrada@twowheelstogo.com>
2022-02-11 16:27:10 -0600

    delete task

--- volume-docker-moodle/admin/tool/airtable/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version   = 2017092120;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012112900;        // Requires this Moodle version
-$plugin->component = 'tool_airtable'; // Full name of the plugin (used for diagnostics)
